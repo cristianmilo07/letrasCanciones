e499fc21335a84de222b6702995dc9bcba64eed3
Cristian Villada <cristianmilo07@gmail.com>
2020-04-09 19:41:24 -0500

    Proyecto terminado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+import { API } from './api.js'
 import * as UI from './interfaz.js';
 
 UI.formularioBuscar.addEventListener('submit', (e) => {
@@ -17,6 +18,26 @@ UI.formularioBuscar.addEventListener('submit', (e) => {
         },3000)
     }else {
         //El formulario esta completo, realizar consulta a la API
+        const api = new API(artista, cancion);
+        api.consultarAPI()
+            .then(data => {
+                console.log(data)
+                if(data.respuesta.lyrics){
+                    console.log('si Existe!')
+                    //La canción si existe
+                    const letra = data.respuesta.lyrics;
+                    UI.divResultado.textContent = letra;
+                }else{
+                    // La canción no existe
+                    UI.divMensajes.innerHTML = 'La canción no existe, prueba con otra búsqueda';
+                    UI.divMensajes.classList.add('error');
+                    setTimeout(() => {
+                        UI.divMensajes.innerHTML = '';
+                        UI.divMensajes.classList.remove('error');
+                        UI.formularioBuscar.reset();
+                    },3000)
+                }
+            })
     }
 
 })
